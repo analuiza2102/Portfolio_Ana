a5b7c8bdc5c54a3208ecd78e5827bb3ab7c055b9
analuiza2102 <aninhaguiiimaraes2102@gmail.com>
2024-03-23 21:47:29 -0300

    feat: configs material ui theme, css baseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+      </CssBaseline>
+      <App />
+    </ThemeProvider>
+   
   </React.StrictMode>,
 )
---
+++ src/theme.tsx
@@ -0,0 +1,32 @@
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+
+
+
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: "#5e5a61",
+    },
+    secondary: {
+      main: "#557174",
+    },
+    background: {
+      default: "#161516",
+    },
+},
+    typography: {
+        fontFamily: 'Helvetica Neue, sans-serif',
+        h1: {
+            color: "#db8286",
+        },
+        h2: {
+            color: "#a4b4bc",
+        },
+    },
+    
+    
+})
+
+theme = responsiveFontSizes(theme);
+export default theme;
